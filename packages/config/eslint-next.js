9b0ff85e0df75127377b4f19ec5dd56e53d3acef
kensiiwasaki <kensiiwasaki@gmail.com>
2023-12-02 19:34:04 +0900

    config

---
+++ packages/config/eslint-next.js
@@ -0,0 +1,80 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'next',
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+    'turbo',
+  ],
+  plugins: ['@typescript-eslint', 'import'],
+  settings: {
+    next: {
+      rootDir: ['apps/*/', 'packages/*/'],
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: ['apps/*/tsconfig.json'],
+      },
+    },
+  },
+  rules: {
+    'turbo/no-undeclared-env-vars': 'error',
+    'prefer-const': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    // react
+    'react/require-default-props': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'react/jsx-props-no-spreading': 'off',
+
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
+
+    // next
+    '@next/next/no-img-element': 'off', // on iOS and Android we can't use next/image
+    '@next/next/no-html-link-for-pages': 'off',
+  },
+  overrides: [
+    {
+      // 3) Now we enable eslint-plugin-testing-library rules or preset only for matching files!
+      env: {
+        jest: true,
+      },
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react', 'plugin:jest/recommended'],
+      rules: {
+        'turbo/no-undeclared-env-vars': 'error',
+
+        'import/no-extraneous-dependencies': [
+          'off',
+          { devDependencies: ['**/?(*.)+(spec|test).[jt]s?(x)'] },
+        ],
+      },
+    },
+  ],
+  ignorePatterns: [
+    '**/*.js',
+    '**/*.json',
+    'node_modules',
+    'public',
+    'styles',
+    '.next',
+    'coverage',
+    'dist',
+    '.turbo',
+  ],
+}
